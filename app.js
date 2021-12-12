de209a805029f8434221954da71176039b8a6781
kelechi prosper <prosperkelechi50@gmail.com>
2021-12-12 10:30:24 +0100

    i

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
-function makeCofee(sugar,milk )
-{
-    var instruction = "boil water,";
-    instruction += " pour into cup,";
-    instruction += " add coffee granules,";
-    instruction += " add " + sugar + " spoons of sugar,";
-    instruction += " add " + sugar + "% milk,";
-    return instruction;
-}
-console.log(makeCofee(2, 20) );
 var car = {
-    color: "red",
-    speed: 300,
-    drive: function(){return "drive"; }
-};
-var shoppinglist = [
-    "Apple",
-    "Banana",
-    "Pineapple",
-]
-var car = {
-    make: "404",
-    speed: 150,
+    make: "lexus",
+    speed: 160,
     engine: {
-        size: 3.2,
-        make: "Ford",
-        fuel: "petrol",
-        pistons: [ { maker: "Ford" }, {maker: "Ford1"} ]
-    }
+        size: 2.0,
+        make: "toyota",
+        fuel: "patrol",
+        pistons: [ {make: "toyota"}, {make: "toyota2"} ]
+
+    },
+    drive: function(){ return "drive"; }
 };
+
+var array = [
+    "string",
+    200,
+    [ "embed", 200],
+    { car: "range rover"},
+    function(){ return "drive"; }
+];
