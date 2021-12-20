6d37f69b869c3ace9b5a95023cc7ac08504db55f
kelechi prosper <prosperkelechi50@gmail.com>
2021-12-20 01:45:28 +0100

    i

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-var car = {
-    make: "lexus",
-    speed: 160,
-    engine: {
-        size: 2.0,
-        make: "toyota",
-        fuel: "patrol",
-        pistons: [ {make: "toyota"}, {make: "toyota2"} ]
+// var car = {
+//     make: "lexus",
+//     speed: 160,
+//     engine: {
+//         size: 2.0,
+//         make: "toyota",
+//         fuel: "patrol",
+//         pistons: [ {maker: "toyota"}, {maker: "toyota2"} ]
 
-    },
-    drive: function(){ return "drive"; }
-};
+//     },
+//     drive: function(){ return "drive"; }
+// };
 
-var array = [
-    "string",
-    200,
-    [ "embed", 200],
-    { car: "range rover"},
-    function(){ return "drive"; }
-];
+// var array = [
+//     "string",
+//     200,
+//     [ "embed", 200],
+//     { car: "range rover"},
+//     function(){ return "drive"; },
+// ];
+// console.log(car.make);
+
+function name()
+{
+    var fullname = "HIGH CHIEF";
+
+    function concat( name )
+    {
+        return"MR." + name;
+    }
+
+    return concat (fullname);
+}
