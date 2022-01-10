366285c8815730367633628177d04113ddad4bcc
kelechi prosper <prosperkelechi50@gmail.com>
2022-01-10 01:37:53 +0100

    i

--- app.js
+++ app.js
@@ -103,9 +103,13 @@
 
 var carsLeft = 0,
 carsRight = 0,
-greenMan = "yes";
+greenMan = "no";
 
 if( greenMan === "yes" )
  {
     console.log('cross the road');
+ }
+ else if(carsLeft === 0 )
+ {
+    console.log(' All clear! cross the road' );
  }
